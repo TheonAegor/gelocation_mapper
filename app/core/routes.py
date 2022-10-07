999bfc636037a153b66f024f6c8d97bd1a6df56b
taegor <kotychka@gmail.com>
2022-10-07 12:30:20 +0300

    added app folder and content

---
+++ app/core/routes.py
@@ -0,0 +1,9 @@
+import os
+from app import BASE_DIR
+from app.base.application import Application
+from app.core.views import IndexView
+
+
+def setup_routes(app: Application):
+    app.router.add_static("/static", os.path.join(BASE_DIR, "client", "static"))
+    app.router.add_view("/", IndexView)
---
+++ app/core/views.py
@@ -0,0 +1,17 @@
+import os
+from aiohttp import web
+from app import BASE_DIR
+from app.base.application import View
+
+
+class IndexView(View):
+    async def get(self):
+        with open(os.path.join(BASE_DIR, "client", "index.html"), "r") as f:
+            file = f.read()
+
+        return web.Response(
+            body=file,
+            headers={
+                "Content-Type": "text/html",
+            },
+        )
